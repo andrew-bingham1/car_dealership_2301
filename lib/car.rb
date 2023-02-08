4d4c6b702843d06d5d1ebf780b72882ced5dbc14
Andrew Bingham <andrewgbingham@gmail.com>
2023-02-08 09:29:07 -0700

    feat: add make and model attributes - tests now pass

--- lib/car.rb
+++ lib/car.rb
@@ -1,10 +1,12 @@
 class Car
-  attr_accessor :make_model, :monthly_payment, :loan_length, :total_cost, :color
+  attr_accessor :make_model, :monthly_payment, :loan_length, :total_cost, :color, :make, :model
   def initialize(make_model, monthly_payment, loan_length)
     @make_model = make_model
     @monthly_payment = monthly_payment
     @loan_length = loan_length
     @total_cost = loan_length * monthly_payment
     @color = nil
+    @make = self.make_model.split(" ").first
+    @model = self.make_model.split(" ").last
   end
 end
