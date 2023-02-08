4a2aadb51862528dae869aa3c4d945757c160342
Andrew Bingham <andrewgbingham@gmail.com>
2023-02-08 09:59:44 -0700

    test: can check inventory test

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -38,5 +38,15 @@ RSpec.describe Dealership do
     expect(dealership.inventory.length).to eq(2)
   end
 
+  it 'can check inventory' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    car_2 = Car.new("Toyota Prius", 1000, 48)
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+
+    expect(dealership.has_inventory?).to eq(true)
+  end
+
 
 end
